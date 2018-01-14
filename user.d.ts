903180ce68f9f458c559fd9115144b91c2a284bc
dalongrong <1141591465@qq.com>
2018-01-14 20:08:50 +0800

    modify d.ts file

--- user.d.ts
+++ user.d.ts
@@ -3,9 +3,7 @@
 //     constructor(name: string);
 //     login(): string;
 // }
-declare module "user"{
-  export = user;
-}
+
 export class user {
     name: string;
     constructor(name: string);
